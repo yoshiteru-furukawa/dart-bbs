2a731b2cbfeba418090688442cc46ffff1b330fb
yoshiteru-furukawa <yfurukawa@g.ecc.u-tokyo.ac.jp>
2023-01-10 13:26:59 +0900

    new suite

--- lib/dart_bbs.dart
+++ lib/dart_bbs.dart
@@ -4,7 +4,8 @@ export 'src/models/get_options.dart';
 export 'src/utils/get_key_pair.dart';
 
 export 'src/vc_create/vc_create.dart';
-export 'src/vc_create/bls_sign.dart';
+export 'src/vc_create/get_proof_value.dart';
 
 export 'src/vp_create/vp_create.dart';
 export 'src/vp_verify/vp_verify.dart';
+export 'src/vp_verify/bls_verify.dart';
